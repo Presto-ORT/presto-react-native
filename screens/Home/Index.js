52d65384018dd90456bb5430ce51543e13f4d295
dtlucho <luciano.ditomaso@gmail.com>
2021-11-11 20:26:59 -0300

    Espacion

--- screens/Home/Index.js
+++ screens/Home/Index.js
@@ -1,34 +1,34 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function Home({navigation}) {   
+export default function Home({ navigation }) {
 
-    const [datos, setDatos] = useState("Hola soy datos")
-   
-    return (
-      <View style={styles.container}>
-          <Text>Hola Mundo desde Home!</Text>
-          <TouchableOpacity 
-          style={styles.button}
-          onPress={() => navigation.navigate("Diario", {datos})}>
-            <Text>Paso mis datos a Details</Text>
-          </TouchableOpacity>
-          
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
+  const [datos, setDatos] = useState("Hola soy datos")
 
-    button: {
-      alignItems: "center",
-      backgroundColor: "#DDDDDD",
-      padding: 10
-    },
-  });
+  return (
+    <View style={styles.container}>
+      <Text>Hola Mundo desde Home!</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Diario", { datos })}>
+        <Text>Agregar gasto</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+});
