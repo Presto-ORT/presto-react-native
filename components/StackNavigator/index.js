1b61ac725f556db44f8fb743ffc5095bebab7ac8
dtlucho <luciano.ditomaso@gmail.com>
2021-11-11 22:58:06 -0300

    Auth flow en navegacion

--- components/StackNavigator/index.js
+++ components/StackNavigator/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import BottomTabsNavigator from "../BottomTabsNavigator";
 import Auth from "../Auth";
+import AddRecord from "../../screens/AddRecord";
+import GlobalContext from "../globals/context";
 
 export default function StackNavigator() {
 
     const Stack = createStackNavigator()
 
+    const [token, setToken] = useState(false);
+
+    const isAuthenticated = () => token;
+
     return (
-        <Stack.Navigator initialRouteName={'Auth'} screenOptions={{ headerShown: false }}>
-            <Stack.Screen name={'Auth'} component={Auth} />
-            <Stack.Screen name={'Main'} component={BottomTabsNavigator} />
-        </Stack.Navigator>
+        <GlobalContext.Provider value={{ token, setToken }}>
+            <Stack.Navigator initialRouteName={'Auth'} screenOptions={{ headerShown: false }}>
+                {
+                    (isAuthenticated())
+                        ? (
+                            <>
+                                <Stack.Screen name={'Main'} component={BottomTabsNavigator} />
+                                <Stack.Screen name={'Agregar registro'} component={AddRecord} options={{ headerShown: true }} />
+                            </>
+                        )
+                        : (
+                            <Stack.Screen name={'Auth'} component={Auth} />
+                        )
+                }
+            </Stack.Navigator>
+        </GlobalContext.Provider>
     );
 }
