a3a11158b58aa4dfce40b904a23ffa4a4e2d0040
Leo Fernandez <31252634@comunidadort.edu.ar>
2021-11-13 12:21:53 -0300

    Componente graficos funcionando

--- screens/Reportes/Index.js
+++ screens/Reportes/Index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { PieChartExample } from './PieChart';
 
 export default function Reportes() {
-
+    const data = [2890.43, 734.17, 8300, 1200,90, 7000]
   
 
     return (
       <View style={styles.container}>
-          <Text>Hola Mundo desde Reportes!</Text>
+          <View style={styles.container}>
+              <PieChartExample tamanio={200} datos={data}/>
+          </View>
+          <View style={styles.list}>
+            <Text style={styles.titulo}>Transporte    $2890   35%</Text>             
+            <Text style={styles.titulo}>Alimentacion  $8300   44%</Text>             
+            <Text style={styles.titulo}>Educacion     $7000   29%</Text>             
+            <Text style={styles.titulo}>Salud         $1200   15%</Text>             
+            <Text style={styles.titulo}>Entretenimiento   $734   4%</Text>             
+          </View>
+          
       </View>
     );
   }
@@ -19,4 +30,17 @@ export default function Reportes() {
       alignItems: 'center',
       justifyContent: 'center'
     },
+    list: {
+        flex: 1,                
+        alignItems: "flex-start",
+        justifyContent: 'center',
+        width:400,
+        paddingBottom: 30,
+        
+    },    
+    titulo: {        
+        padding: 10       
+        
+    },
+    
   });
