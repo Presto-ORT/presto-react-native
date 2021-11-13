6a455e29d778eb8aeb243624fabcb04b49c197c7
fermorello <fmorello@abs-ti.com>
2021-11-13 03:27:25 -0300

    Ejemplo de llamada de servicio a travez de la capa api

--- api/users.js
+++ api/users.js
@@ -1,3 +1,8 @@
 import axios from "axios";
 
+const getExample = async () => {
+    const url = "https://api2.binance.com/api/v3/ticker/24hr";
+    return await axios.get(url)
+}
 
+export { getExample }
