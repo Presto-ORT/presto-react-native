d55554c1016e9f2feac85bbb5afcaa58dd0eb918
fermorello <fmorello@abs-ti.com>
2021-11-12 23:54:44 -0300

    Estructura para consumir servicios

--- screens/AddRecord/index.js
+++ screens/AddRecord/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getExample } from '../../api/registros'
 
 export default function AddRecord({ navigation }) {
 
