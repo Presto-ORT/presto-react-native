88f69fff5983b8bf46c6f1226d4aece8cd611b45
Leo Fernandez <31252634@comunidadort.edu.ar>
2021-11-12 21:00:25 -0300

    Estructura userArea & top5Categ

--- screens/Home/Index.js
+++ screens/Home/Index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, Image, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Home({ navigation }) {
 
@@ -7,40 +8,52 @@ export default function Home({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo desde Home!</Text>
+      <View style={styles.userArea} >
+        <Image style={styles.userImage} source={require('../../Data/images/lionel-messi.jpg')}/>
+        
+      </View>
 
       <View style={styles.topFive}>
         <View style={styles.topFiveHeader}>
           <Text>Top 5 de categorias</Text>
-          <Text>mayor gasto</Text>
+          <Text style={styles.title}>mayor gasto</Text>
         </View>
         <View style={styles.topFiveCategory}>
-          <Text>Category name</Text>
+          <View style={styles.categoryTitle}>            
+            <Ionicons name="bus-outline" size={20} color="#000" />  
+            <Text style={styles.title}>Transporte</Text>
+          </View>          
           <Text>$ 100</Text>
         </View>
         <View style={styles.topFiveCategory}>
-          <Text>Category name</Text>
+          <View style={styles.categoryTitle}>            
+            <Ionicons name="pizza-outline" size={20} color="#000" />  
+            <Text style={styles.title}>Alimentacion</Text>
+          </View >  
           <Text>$ 100</Text>
         </View>
         <View style={styles.topFiveCategory}>
-          <Text>Category name</Text>
+          <View style={styles.categoryTitle}>            
+            <Ionicons name="school-outline" size={20} color="#000" />  
+            <Text style={styles.title}>Educacion</Text>
+          </View>  
           <Text>$ 100</Text>
         </View>
         <View style={styles.topFiveCategory}>
-          <Text>Category name</Text>
+          <View style={styles.categoryTitle}>
+            <Ionicons name="game-controller-outline" size={20} color="#000" />  
+            <Text style={styles.title}>Entretenimiento</Text>            
+          </View>  
           <Text>$ 100</Text>
         </View>
         <View style={styles.topFiveCategory}>
-          <Text>Category name</Text>
+          <View style={styles.categoryTitle}>
+            <Ionicons name="flash-outline" size={20} color="#000" styles={styles.icon}/>  
+            <Text style={styles.title}>Servicios</Text>            
+          </View>  
           <Text>$ 100</Text>
         </View>
       </View>
-
-      {/* <TouchableOpacity
-        style={styles.button}
-        onPress={() => { }}>
-        <Text>Agregar registro</Text>
-      </TouchableOpacity> */}
 
     </View>
   );
@@ -49,39 +62,55 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2b7bcc',
     alignItems: 'center',
     justifyContent: 'center'
   },
 
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
   topFive: {
+    flex: 1,
     width: Dimensions.get('window').width,
     backgroundColor: "#DDDDDD",
-    borderColor: "#000000",
-    borderWidth: 2,
+    borderColor: "#000000",    
+    justifyContent: "space-between",
+    paddingBottom: 10
+  },
+
+  userArea: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    backgroundColor: "#2b7bcc",  
+    alignItems: 'center',
+    justifyContent: 'center'    
   },
 
   topFiveHeader: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    marginVertical: 5,
-    paddingHorizontal: 2.5,
-    paddingVertical: 5,
+    justifyContent: "space-between",  
     backgroundColor: "#FFF"
   },
 
   topFiveCategory: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    marginHorizontal: 10,
-    marginVertical: 5,
-    paddingHorizontal: 2.5,
-    paddingVertical: 5,
-    backgroundColor: "#FFF"
+    justifyContent: "space-between",    
+    padding: 10,
+    backgroundColor: "#FFF",
+    marginBottom: 10
   },
+
+  userImage: {    
+    height: 200,
+    width: 200,
+    borderRadius: 180
+  },
+
+  categoryTitle: {    
+    flexDirection: 'row'     
+  },
+
+  title: {    
+    paddingLeft: 10,
+    fontSize: 15
+  },
+
 });
