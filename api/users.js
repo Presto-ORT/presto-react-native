28c9dd23d8ae246c8514883339034398b4550c8d
dtlucho <luciano.ditomaso@gmail.com>
2021-11-15 01:34:48 -0300

    [FT] Basic log in api connection

--- api/users.js
+++ api/users.js
@@ -1,8 +1,26 @@
 import axios from "axios";
+import { Value } from "react-native-reanimated";
 
 const getExample = async () => {
     const url = "https://api2.binance.com/api/v3/ticker/24hr";
     return await axios.get(url)
 }
 
-export { getExample }
+const register = async (name, email, password) => {
+    const url = "http://localhost:3000/users/register"
+    let response = await axios.post(url, { name, email, password });
+    console.log(response.data);
+}
+
+const login = async (email, password) => {
+
+    console.log('press login api');
+    const url = "http://192.168.0.28:3000/users/login"
+    console.log(email, password);
+
+    let response = await axios.post(url, { email, password });
+
+    return response.data;
+}
+
+export { getExample, login }
