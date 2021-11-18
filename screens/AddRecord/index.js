798d54b9e49859023e825efc8b51c35cc1f0b6c1
fermorello <fmorello@abs-ti.com>
2021-11-18 00:37:14 -0300

    Formulario terminado

--- screens/AddRecord/index.js
+++ screens/AddRecord/index.js
@@ -14,17 +14,15 @@ import {
     Button,
     Dimensions,
     Platform,
-    Keyboard
+    Keyboard,
+    ActivityIndicator
 } from 'react-native';
-
-
-
 
 export default function AddRecord({ navigation }) {
 
     const [amount, setAmount] = useState();
     const [description, setDescription] = useState();
-    const [expense, setExpense] = useState();
+    const [expense, setExpense] = useState(true);
     const [category, setCategory] = useState();
     const [subCategory, setSubCategory] = useState();
     const [date, setDate] = useState(new Date());
@@ -34,18 +32,24 @@ export default function AddRecord({ navigation }) {
     const [user, setUser] = useState();
     const [show, setShow] = useState(false);
     const [categories, setCategories] = useState([]);
-
+    const [loading, setLoading] = useState(true);
+    const [errorAmount, setErrorAmount] = useState()
 
 
     useEffect(async () => {
         try {
             let response = await getCategories();
             setCategories(response);
+            setCategory(response[0].category)
+            setSubCategory(response[0].subcategory[0])
+            setLoading(false);
         } catch {
 
         }
 
     }, [])
+
+
 
     const handleChangeDate = (e, selectedDate) => {
         setShow(Platform.OS === 'ios');
@@ -69,107 +73,142 @@ export default function AddRecord({ navigation }) {
         showMode('date');
     };
 
+    const handleAmount = e => {
+        const hasNumber = new RegExp(/^-?\d+\.?\d*$/);
+
+        if(e <= 0){
+            setAmount(null);
+            setErrorAmount("El valor no puede ser igual o menor a 0.");
+        }else if(!hasNumber.test(e)){
+            setAmount(null);
+            setErrorAmount("Solo puede ingresar números.");
+        }else{
+            setAmount(e);
+            setErrorAmount("");
+        }
+    }
+
     const handleEnviarRegistro = async () => {
         const registro = {
             date,
             category,
-            subCategory,
+            sub: subCategory,
             amount,
-            pesos,
+            dolar: !pesos,
             description,
+            expense
         }
 
         if (!registro.date || !registro.category || !registro.subCategory || !registro.amount) return;
 
-        await saveRecord(registro);
-        navigation.navigate('Diario');
+        try{
+            await saveRecord(registro);
+            console.log("se registró")
+            navigation.navigate('Diario');
+        }catch{
+            console.log("No se pudo registrar.")
+        }
     }
 
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.formContainer}>
-                <View>
-                    <TouchableHighlight>
-                        <Text
-                            style={styles.textInput}
-                            onPress={showDatepicker}>
-                            {dateToShow == "" ? "Ingresar fecha del gasto" : dateToShow}
-                        </Text>
-                    </TouchableHighlight>
-                </View>
-                {show && <DateTimePicker
-                    style={styles.width}
-                    maximumDate={new Date()}
-                    testID="dateTimePicker"
-                    value={date}
-                    mode={"date"}
-                    is24Hour={true}
-                    display="default"
-                    onChange={handleChangeDate}
-                />
-                }
-                <TouchableWithoutFeedback onPress={() => Keyboard.dismis}>
-                    <Picker
-                        style={styles.picker, styles.width}
-                        selectedValue={category}
-                        onValueChange={(itemValue) => setCategory(itemValue)}
-                    >
-                        {categories.length > 0 && categories.map((c, i) =>
-                            <Picker.Item
-                                key={i}
-                                placeholder="Ingrese una categoria"
-                                label={c.title}
-                                value={c.title} />
-                        )}
-                    </Picker>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback>
-                    <Picker
-                        style={styles.picker, styles.width}
-                        selectedValue={subCategory}
-                        onValueChange={(itemValue) => setSubCategory(itemValue)}
-                    >
-                        {category &&
-                            categories.filter(c => c.title === category)[0].subcategory.map((fc, i) =>
-                                <Picker.Item key={i} label={fc.title} value={fc.title} />
-                            )
-                        }
-                    </Picker>
-                </TouchableWithoutFeedback>
-                <View style={styles.width, styles.montoTipo}>
+    if(loading){
+        return(
+        <View style={[styles.container, styles.horizontal]}>
+            <ActivityIndicator size="large" color="#0000ff"/>
+        </View>)
+    }else{
+        return (
+            
+            <View style={styles.container}>
+                
+                <View style={styles.formContainer}>
+                    <View>
+                        <TouchableHighlight>
+                            <Text
+                                style={[styles.textInput]}
+                                onPress={showDatepicker}>
+                                {dateToShow == "" ? "Ingresar fecha del gasto" : dateToShow}
+                            </Text>
+                        </TouchableHighlight>
+                    </View>
+                    {show && <DateTimePicker
+                        style={styles.width}
+                        maximumDate={new Date()}
+                        testID="dateTimePicker"
+                        value={date}
+                        mode={"date"}
+                        is24Hour={true}
+                        display="default"
+                        onChange={handleChangeDate}
+                        maxDate = {new Date()}
+                    />
+                    }
+                    <TouchableWithoutFeedback onPress={() => Keyboard.dismis}>
+                        <Picker
+                            style={[styles.picker, styles.width]}
+                            selectedValue={category}
+                            onValueChange={(itemValue) => setCategory(itemValue)}
+                        >
+                            {categories.length > 0 && categories.map((c,i) =>
+                                <Picker.Item
+                                    label={c.title}
+                                    value={c.title} 
+                                    key={i}/>
+                            )}
+                        </Picker>
+                    </TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback>
+                        <Picker
+                            style={[styles.picker, styles.width]}
+                            selectedValue={subCategory}
+                            onValueChange={(itemValue) => setSubCategory(itemValue)}
+                        >
+                            {category &&
+                                categories.filter(c => c.title === category)[0].subcategory.map((fc,i) =>
+                                    <Picker.Item 
+                                        label={fc.title} 
+                                        value={fc.title} 
+                                        key={i} />
+                                )
+                            }
+                        </Picker>
+                    </TouchableWithoutFeedback>
+                    <View style={styles.width, styles.montoTipo}>
+                        <TextInput
+                            style={[styles.textInput]}
+                            placeholder="Ingresar el valor"
+                            value={amount}
+                            keyboardType="numeric"
+                            onChangeText={(value) => handleAmount(value)}
+                        />
+                        <Switch
+                            style = {styles.borderBottom}
+                            trackColor={{ false: '#a0a3a8', true: '#a0a3a8' }}
+                            thumbColor={pesos ? '#4a628a' : '#f4f3f4'}
+                            ios_backgroundColor="#3e3e3e"
+                            onChange={() => { setPesos(!pesos) }}
+                            value={pesos}
+    
+                        />
+                    </View>
+                    {errorAmount !== "" && <Text style={{marginBottom:25, color:"red"}}> {errorAmount} </Text>}
                     <TextInput
-                        style={styles.textInput}
-                        placeholder="Ingresar el valor"
-                        value={amount}
-                        keyboardType="numeric"
-                        onChangeText={(value) => setAmount(value)}
+                        style={[styles.width, styles.textInput]}
+                        placeholder="Ingresar la descripción"
+                        value={description}
+                        multiline={true}
+                        onChangeText={(value) => setDescription(value)}
                     />
-                    <Switch
-                        trackColor={{ false: '#a0a3a8', true: '#a0a3a8' }}
-                        thumbColor={pesos ? '#4a628a' : '#f4f3f4'}
-                        ios_backgroundColor="#3e3e3e"
-                        onChange={() => { setPesos(!pesos) }}
-                        value={pesos}
-
+                    <Button
+                        title="Ingresar gasto"
+                        color="#4a628a"
+                        onPress={handleEnviarRegistro}
+                        disabled={!date || !category || !subCategory || !amount}
                     />
                 </View>
-                <TextInput
-                    style={styles.width, styles.textInput, styles.inputDescripcion}
-                    placeholder="Ingresar la descripción"
-                    value={description}
-                    multiline={true}
-                    onChangeText={(value) => setDescription(value)}
-                />
-                <Button
-                    title="Ingresar gasto"
-                    color="#4a628a"
-                    onPress={handleEnviarRegistro}
-                />
             </View>
-        </View>
-    );
+        );
+    }
+    
 
 }
 
@@ -179,6 +218,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#fff',
+        paddingTop:30
     },
     formContainer: {
         padding: 8,
@@ -192,16 +232,15 @@ const styles = StyleSheet.create({
     },
     picker: {
         height: 25,
-        width: 100
+        marginBottom: 30
     },
     montoTipo: {
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
     textInput: {
-        fontSize: 16
+        fontSize: 16,
+        marginLeft: 10,
+        marginBottom: 30,
     },
-    inputDescripcion: {
-        height: 110
-    }
 });
